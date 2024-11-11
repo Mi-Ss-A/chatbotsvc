d68481de3ae0013aeb6618d5ddff862c2fcbf172
seungji2001 <lisa2009343123456@gmail.com>
2024-11-11 16:50:08 +0900

    feat: redis 설정 및 추가

---
+++ src/main/java/com/wibeechat/missa/config/SessionConfig.java
@@ -0,0 +1,21 @@
+package com.wibeechat.missa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+// SessionConfig.java
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+}
